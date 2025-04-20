39c1ed9235815e6b438189c672abc4a9efcb59c9
Aldair Cruz Bautista <aldair.cruzbautista@outlook.com>
2025-04-19 22:20:34 -0600

    Creacion de la pagina de contacto

---
+++ src/views/ContactView.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import FooterComponent from '../components/FooterComponent.vue';
+	import HeaderComponent from '../components/HeaderComponent.vue';
+    import ContactComponent from '../components/ContactComponent.vue';
+
+</script>
+
+<template>
+	<HeaderComponent />
+    <main class="pt-3 pb-3">
+        <ContactComponent/>
+    </main>
+	<FooterComponent />
+</template>
+
+<style scoped>
+</style>
