bd63ca78c9ff7fe740e1938534169eb07d31b159
Aldair Cruz Bautista <aldair.cruzbautista@outlook.com>
2025-04-21 22:53:43 -0600

    Estilizado al footer del sitio web

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,41 +2,48 @@
 </script>
 
 <template>
-<footer class="bg-white mt-5 py-4">
+<footer class="bg-footer mt-5 py-4">
   <div class="container">
     <div class="row text-center text-md-start">
       <!-- Columna de contacto -->
       <div class="col-md-4 mb-4 mb-md-0 border-end">
-        <ul class="list-unstyled">
-          <li><a href="#" class="text-decoration-underline text-dark">TELÉFONO</a></li>
-          <li><a href="#" class="text-decoration-underline text-dark">WHATSAPP</a></li>
-          <li><a href="#" class="text-decoration-underline text-dark">CORREO</a></li>
-          <li><a href="#" class="text-decoration-underline text-dark">DIRECCIÓN Y HORARIOS</a></li>
+        <ul class="list-unstyled fs-5 mt-3">
+          <li><a href="#" class="text-decoration-none color-footer">477 628 7515</a></li>
+          <li><a href="#" class="text-decoration-none color-footer">477 821 0020</a></li>
+          <li class="mt-3 mb-3"><a href="#" class="text-decoration-none color-footer">contacto@terroacabados.com</a></li>
+          <li>
+            <a href="#" class="text-decoration-none color-footer">
+              Blvd. J. J. Torres Landa Ote. 5642,
+              Cerrito de Jerez, 37530 León de los
+              Aldama, Gto.
+            </a>
+          </li>
         </ul>
       </div>
 
       <!-- Columna de políticas -->
       <div class="col-md-4 mb-4 mb-md-0 border-end">
-        <ul class="list-unstyled">
-          <li><a href="#" class="text-decoration-underline text-dark">FAQ´s</a></li>
-          <li><a href="#" class="text-decoration-underline text-dark">Políticas de devolución</a></li>
-          <li><a href="#" class="text-decoration-underline text-dark">Política de privacidad</a></li>
-          <li><a href="#" class="text-decoration-underline text-dark">Términos y condiciones</a></li>
+        <ul class="list-unstyled ms-lg-3 fs-5 mt-3">
+          <li class="mb-3"><a href="#" class="text-decoration-none color-footer">FAQ´s</a></li>
+          <li class="mb-3"><a href="#" class="text-decoration-none text-uppercase color-footer">Aviso de Privacidad</a></li>
+          <li class="mb-3"><a href="#" class="text-decoration-none text-uppercase color-footer">Políticas de Entrega y Devoluciones</a></li>
+          <li class="mb-3"><a href="#" class="text-decoration-none text-uppercase color-footer">Términos y condiciones</a></li>
         </ul>
       </div>
 
       <!-- Columna de redes sociales -->
       <div class="col-md-4 d-flex flex-column align-items-center justify-content-center">
         <div class="d-flex gap-3">
-          <a href="#" class="text-dark fs-4"><i class="bi bi-facebook fs-1 ms-3"></i></a>
-          <a href="#" class="text-dark fs-4"><i class="bi bi-instagram fs-1 mx-3"></i></a>
-          <a href="#" class="text-dark fs-4"><i class="bi bi-whatsapp fs-1 me-3"></i></a>
+          <a href="#" class="text-dark fs-4"><i class="icon-footer bi bi-facebook fs-1 ms-3"></i></a>
+          <a href="#" class="text-dark fs-4"><i class="icon-footer bi bi-instagram fs-1 mx-3"></i></a>
+          <a href="#" class="text-dark fs-4"><i class="icon-footer bi bi-pinterest fs-1 me-3"></i></a>
+          <a href="#" class="text-dark fs-4"><i class="icon-footer bi bi-youtube fs-1 me-3"></i></a>
         </div>
       </div>
     </div>
 
     <!-- Pie de página -->
-    <div class="text-center mt-4 small text-muted">
+    <div class="text-center mt-4 fs-6 color-footer">
       ©2025 por Tejas y barro de León.
     </div>
   </div>
@@ -44,5 +51,14 @@
 </template>
 
 <style scoped>
-
-</style>
+  .bg-footer {
+    background-color: #fff8ea !important;
+  }
+  .border-end {
+    border-right: 1px solid #454015 !important;
+  }
+  .color-footer,
+  .icon-footer{
+    color: #454015 !important;
+  }
+  </style>
