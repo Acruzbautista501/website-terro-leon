00245b5d39178a5e8bb299223485d4000c38b457
Aldair Cruz Bautista <aldair.cruzbautista@outlook.com>
2025-04-29 00:31:37 -0600

    Enrutamiento de pagina productos y tejas

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,10 +3,11 @@ import type { RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import ContactView from '../views/ContactView.vue'
+import ProductsView from '../views/ProductsView.vue'
 import DidacticView from '../views/DidacticView.vue'
 import TermsConditionsView from '../views/TermsConditionsView.vue'
 import FAQsView from '../views/FAQsView.vue'
-
+import TejasView from '../views/products/TejasView.vue'
 
 const routes: RouteRecordRaw[] = [
   {
@@ -25,6 +26,16 @@ const routes: RouteRecordRaw[] = [
     component: ContactView,
   },
   {
+    path: '/productos',
+    name: 'Productos',
+    component: ProductsView,
+  },
+  {
+    path: '/productos/tejas',
+    name: 'Tejas',
+    component: TejasView,
+  },
+  {
     path: '/recursos',
     name: 'Recursos',
     component: DidacticView,
@@ -39,8 +50,7 @@ const routes: RouteRecordRaw[] = [
     name: 'FAQ´s',
     component: FAQsView,
   },
-]
-
+];
 const router = createRouter({
   history: createWebHistory(),
   routes
